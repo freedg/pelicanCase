19bba51267224cc831cf81b1a346578d2ebf6f68
freedg <freedg@users.noreply.github.com>
2016-04-28 22:12:13 -0500

    id numbers instead of typing out our name

---
+++ dictionary.php
@@ -0,0 +1,18 @@
+<?php
+$idDictionary = [
+  "1000092465" => "Cara Kloes",
+  "1000094677" => "Sam Sinclair",
+  "1000446503" => "Caleb Slayton",
+  "1000485122" => "Aaron Gehring",
+  "1000512870" => "Lexy Vancuyk",
+  "1000513033" => "Larissa Davis", 
+  "1000513034" => "Jordan Wendt",
+  "1000514148" => "Gabrielle Davis",
+  "1000515238" => "Katherine Veum",
+  "1000523415" => "Micah Gehring",
+  "1000536656" => "Max Hinrichs",
+  "1000546003" => "Jimmy Herrmann",
+  "1000546998" => "Kylie Sullivan",
+  "1008108607" => "Isaac Slayton",
+];
+?>
